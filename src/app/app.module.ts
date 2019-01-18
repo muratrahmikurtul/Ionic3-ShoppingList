eefcc93326c42f8817fe4f35ab8377836c9092d2
muratrahmikurtul <muratrahmikurtul@gmail.com>
2019-01-18 15:39:43 +0300

    Add ShoppingItem Model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,12 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule} from 'angularfire2/database'
+
 
 import { MyApp } from './app.component';
+import { FIREBASE_CONFIG } from './firebase.credentials';
 
 
 @NgModule({
@@ -13,7 +17,9 @@ import { MyApp } from './app.component';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [

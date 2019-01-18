a890cb3e5cea935f159817dd30d78e5147a7f227
muratrahmikurtul <muratrahmikurtul@gmail.com>
2019-01-18 15:20:17 +0300

    Configure FireBase Settings

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyA-D6h-8jKt64u_ZR6aAAUjWNdjHuxz2c0",
+    authDomain: "ionic3-shoppinglist-d6720.firebaseapp.com",
+    databaseURL: "https://ionic3-shoppinglist-d6720.firebaseio.com",
+    projectId: "ionic3-shoppinglist-d6720",
+    storageBucket: "",
+    messagingSenderId: "485183795363"
+  };

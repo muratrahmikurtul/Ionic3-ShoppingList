0572476ac44f49226e24d86592e0b913aac9751a
muratrahmikurtul <muratrahmikurtul@gmail.com>
2019-01-18 16:24:14 +0300

    Display Shopping Items and Start Edit Page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { ShoppingListService } from '../../services/shopping-list/shopping-list.service';
+import { Item } from '../../models/item/item.model';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
 
 @IonicPage()
 @Component({
@@ -8,8 +12,17 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  shoppingList$: Observable<Item[]>
 
+  constructor(public navCtrl: NavController, private shopping: ShoppingListService) {
+    this.shoppingList$ = this.shopping
+      .getShoppingList() // DB LIST
+      .snapshotChanges() // Key and Value
+      .map(
+      changes => {
+        return changes.map(c => ({
+          key: c.payload.key, ...c.payload.val()
+        }));
+      });
   }
-
 }
